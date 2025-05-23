c0b9fadbfe6228462a2ff39b0837599c6c5c3499
jahong1r-t <jahongirtorayev1507@gmail.com>
2025-05-23 14:41:31 +0500

    Auth logic add

--- src/main/java/com/example/ticket/contoller/AddEventController.java
+++ src/main/java/com/example/ticket/contoller/AddEventController.java
@@ -8,10 +8,16 @@ import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
 
+import static com.example.ticket.utils.Util.isSessionValid;
+
 @WebServlet("/event")
 public class AddEventController extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        req.getRequestDispatcher("/dashboard/pages/add.jsp").forward(req, resp);
+        if (isSessionValid(req)) {
+            req.getRequestDispatcher("/dashboard/pages/add.jsp").forward(req, resp);
+        } else {
+            resp.sendRedirect("/login");
+        }
     }
 }
